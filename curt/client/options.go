5bb0b9a01a731fcb8e927575f7a5d3caf4a2a841
Alex Ninneman <alex@ninneman.org>
2017-11-11 00:46:51 -0600

    implements build requirement for query string params

--- curt/client/options.go
+++ curt/client/options.go
@@ -3,8 +3,15 @@ package client
 // Options defines per-request parameters for
 // the client to use on execution.
 type Options struct {
+	Method      string
 	Endpoint    string
 	Result      interface{}
 	Body        interface{}
-	QueryString map[string]string
+	QueryString Params
+}
+
+// Params specifies the requirement for parsing a series
+// of parameters into a Query String.
+type Params interface {
+	Build() (map[string]string, error)
 }
