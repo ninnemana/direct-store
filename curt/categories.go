c95488c81183b9fe828a90c750f4d86794fb3354
Alex Ninneman <alex@ninneman.org>
2017-11-11 00:47:10 -0600

    defines the category service requirements and category definition

--- curt/categories.go
+++ curt/categories.go
@@ -3,38 +3,39 @@ package curt
 import (
 	"net/url"
 	"time"
+
+	"github.com/curt-labs/API/models/brand"
 )
 
 type Categories interface {
-	List() (interface{}, error)
-	Get() (interface{}, error)
+	List() ([]Category, error)
+	Get() (*Category, error)
+	GetParts() (interface{}, error)
 }
 
 type Category struct {
-	ID       int        `json:"id"`
-	ParentID int        `json:"parent_id"`
-	Children []Category `json:"children"`
+	ID       int `json:"id"`
+	ParentID int `json:"parent_id"`
 
-	Sort            int                      `json:"sort"`
-	DateAdded       time.Time                `json:"date_added"`
-	Title           string                   `json:"title"`
-	ShortDesc       string                   `json:"short_description"`
-	LongDesc        string                   `json:"long_description"`
-	ColorCode       string                   `json:"color_code"`
-	FontCode        string                   `json:"font_code"`
-	Image           *url.URL                 `json:"image"`
-	Icon            *url.URL                 `json:"icon"`
-	IsLifestyle     bool                     `json:"lifestyle"`
-	VehicleSpecific bool                     `json:"vehicle_specific"`
-	VehicleRequired bool                     `json:"vehicle_required"`
-	MetaTitle       string                   `json:"meta_title"`
-	MetaDescription string                   `json:"meta_description"`
-	MetaKeywords    string                   `json:"meta_keywords"`
-	Content         []Content                `json:"content"`
-	Videos          []Video                  `json:"videos"`
-	PartIDs         []int                    `json:"part_ids"`
-	Brand           Brand                    `json:"brand"`
-	ProductListing  *PaginatedProductListing `json:"product_listing"`
-	PDFpath         *url.URL                 `json:"pdf_path"`
-	XLSpath         *url.URL                 `json:"xls_path"`
+	Sort            int         `json:"sort"`
+	DateAdded       time.Time   `json:"date_added"`
+	Title           string      `json:"title"`
+	ShortDesc       string      `json:"short_description"`
+	LongDesc        string      `json:"long_description"`
+	ColorCode       string      `json:"color_code"`
+	FontCode        string      `json:"font_code"`
+	Image           *url.URL    `json:"image"`
+	Icon            *url.URL    `json:"icon"`
+	IsLifestyle     bool        `json:"lifestyle"`
+	VehicleSpecific bool        `json:"vehicle_specific"`
+	VehicleRequired bool        `json:"vehicle_required"`
+	MetaTitle       string      `json:"meta_title"`
+	MetaDescription string      `json:"meta_description"`
+	MetaKeywords    string      `json:"meta_keywords"`
+	Content         []Content   `json:"content"`
+	Videos          []Video     `json:"videos"`
+	PartIDs         []int       `json:"part_ids"`
+	Brand           brand.Brand `json:"brand"`
+	PDFpath         *url.URL    `json:"pdf_path"`
+	XLSpath         *url.URL    `json:"xls_path"`
 }
